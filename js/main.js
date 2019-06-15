f5e13d7aadbc2cbc5b163e09de5034599b3c8843
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-06-15 17:38:09 -0300

    finishing loading json data, working on frontEnd for section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
 const getData = async () => {
-  const res = await fetch('./../db/data.json');
-  const data = await res.json();
-  console.log(data);
+  const req = await fetch('./../db/data.json');
+  const res = await req.json();  
+  return res;
 }
 
-getData();
+const data = getData();
+data.then(function(user_data){
+	let id = 0;
+	$(user_data).each(function() {
+		id++;
+		this.id = id;
+		$("#users").append(
+		  `<div class="card card-align bg-white">
+	      <img src="${this.foto}" alt="">
+	      <div>
+	        <p>
+	          <b>${this.nome}</b><br />
+	          ${this.cargo}
+	        </p>
+	      </div>
+	    </div>`
+		);
+	});	  
+
+	$("#selected-user").prepend(
+		`<div class="card card-align bg-white">
+      <img src="${this.foto}" alt="">
+      <div>
+        <p>
+          <b>${this.nome}</b><br />
+          ${this.cargo}
+        </p>
+      </div>
+    </div>`
+	);
+});
+
+
